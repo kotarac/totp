f36f9f3f954d9b602fb2fdd0af94715a69002db7
5-pebbles <5-pebble@protonmail.com>
2024-10-01 09:40:18 -0400

    refactor(base32): use new version;

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,60 @@
-extern crate base32;
 extern crate oath;
 
-use std::io::{self,Write};
+use base32;
 use std::env;
+use std::io::{self, Write};
 use std::process;
 
 fn totp(secret: &str) -> Result<u64, &'static str> {
-	let secret_bytes = base32::decode(base32::Alphabet::RFC4648 {padding: false}, secret).ok_or("invalid base32")?;
-	let code: u64 = oath::totp_raw(&secret_bytes, 6, 0, 30);
-	Ok(code)
+    let secret_bytes = base32::decode(base32::Alphabet::Rfc4648 { padding: false }, secret)
+        .ok_or("invalid base32")?;
+    let code: u64 = oath::totp_raw(&secret_bytes, 6, 0, 30);
+    Ok(code)
 }
 
 fn stdin() -> Result<String, &'static str> {
-	let mut input = String::new();
-	match io::stdin().read_line(&mut input) {
-		Ok(_) => {
-			let input: String = input.trim().to_string();
-			Ok(input)
-		},
-		Err(_) => return Err("error reading stdin"),
-	}
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let input: String = input.trim().to_string();
+            Ok(input)
+        }
+        Err(_) => return Err("error reading stdin"),
+    }
 }
 
 fn error(err: &str) {
-	writeln!(&mut ::std::io::stderr(), "error: {}, try --help", err).unwrap();
-	process::exit(1);
+    writeln!(&mut ::std::io::stderr(), "error: {}, try --help", err).unwrap();
+    process::exit(1);
 }
 
 fn help() {
-	println!("usage with an argument: totp <base32 secret>");
-	println!("usage reading from stdin: echo <base32 secret> | totp");
+    println!("usage with an argument: totp <base32 secret>");
+    println!("usage reading from stdin: echo <base32 secret> | totp");
 }
 
 fn handle(secret: &str) {
-	match totp(secret) {
-		Ok(code) => println!("{:06}", code),
-		Err(err) => error(err.to_string().as_ref()),
-	}
+    match totp(secret) {
+        Ok(code) => println!("{:06}", code),
+        Err(err) => error(err.to_string().as_ref()),
+    }
 }
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-	match args.len() {
-		1 => {
-			match stdin() {
-				Ok(input) => handle(input.as_ref()),
-				Err(err) => error(err.to_string().as_ref()),
-			}
-		},
-		2 => {
-			match args[1].as_ref() {
-				"-h" => help(),
-				"--help" => help(),
-				_ => handle(args[1].as_ref()),
-			}
-		},
-		_ => {
-			error("invalid usage");
-		}
-	}
+    match args.len() {
+        1 => match stdin() {
+            Ok(input) => handle(input.as_ref()),
+            Err(err) => error(err.to_string().as_ref()),
+        },
+        2 => match args[1].as_ref() {
+            "-h" => help(),
+            "--help" => help(),
+            _ => handle(args[1].as_ref()),
+        },
+        _ => {
+            error("invalid usage");
+        }
+    }
 }
